fddd93b93279eae875f6a6aa8dec8b305843fad6
Joanne Greenhalgh <Joanne.Greenhalgh@roq.local>
2023-02-07 15:53:22 +0000

    first commit

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -6,7 +6,43 @@ beforeEach(() => {
     fibonacci = new Fibonacci();
 });
 
-test('When initialized the next number can be returned', () => {
+test('next(): When initialized the next number can be returned', () => {
     fibonacci.init(8);
     expect(fibonacci.next()).toBe(13);
+});
+
+test('next(): When not initialised, next number is returned', () => {
+    expect(fibonacci.next()).toBe(1);
+});
+
+// test('next(): When initialized with a non-fibonacci number an error is thrown', () => {
+//     fibonacci.init(7); //code will fail at this point! will never get to next line
+//     expect(() => {
+//         fibonacci.next();
+//     }).toThrow();
+// });
+
+test('init(seed): can be initialised to a fibonacci number', () => {
+    expect(fibonacci.init(8)).toBe(8);
+});
+
+test('init(seed): can not be initialised to a non-fibonacci number', () => {
+    expect(() => {
+        fibonacci.init(7);
+    }).toThrow('Not a fibonacci number');
+});
+
+test('skip(forward): can be skipped by 1 to a fibonacci number', () => {
+    fibonacci.init(8);
+    expect(fibonacci.skip(1)).toBe(13);
+});
+
+test('skip(forward): can be skipped by 2 to a fibonacci number', () => {
+    fibonacci.init(8);
+    expect(fibonacci.skip(2)).toBe(21);
+});
+
+test('skip(forward): can be skipped by -1 to a fibonacci number', () => {
+    fibonacci.init(8);
+    expect(fibonacci.skip(-1)).toBe(5);
 });
